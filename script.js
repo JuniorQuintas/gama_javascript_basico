4555b3174b0cb49b768e709f01153d8627458e98
JuniorQ <juniorq.dev.git@gmail.com>
2021-07-09 08:22:30 -0300

    incluido o arquivo script no index e criando o primeiro exercício - fizzBuzz

--- script.js
+++ script.js
@@ -4,3 +4,13 @@
 // divisível por 3 e 5 - 'FizzBuzz'
 // não é número - "Não é um número"
 // não é divisível por 3 nem por 5 - entrada
+
+function fizzBuzz(entrada) {
+  if (typeof entrada !== "number") return "Não é um número";
+  if (entrada % 3 === 0 && entrada % 5 === 0) return "FizzBuzz";
+  if (entrada % 3 === 0) return "Fizz";
+  if (entrada % 5 === 0) return "Buzz";
+  return entrada;
+}
+let resultado = fizzBuzz(14);
+console.log(resultado);
