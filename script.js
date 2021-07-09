51106b3583ce668c0448b976c0a6d146858a7568
JuniorQ <juniorq.dev.git@gmail.com>
2021-07-09 07:48:09 -0300

    Adicionado os arquivos index.html e script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// fizzBuzz
+// divisível por 3 - "Fizz"
+// divisível por 5 - "Buzz"
+// divisível por 3 e 5 - 'FizzBuzz'
+// não é número - "Não é um número"
+// não é divisível por 3 nem por 5 - entrada
