187b44ec5e3b0402e950a444238234ae1c701d1b
JuniorQ <juniorq.dev.git@gmail.com>
2021-07-10 21:25:59 -0300

    tentativa de corrigir o código mas ainda não consegui

--- script.js
+++ script.js
@@ -5,33 +5,33 @@
 // não é número - "Não é um número"
 // não é divisível por 3 nem por 5 - entrada
 
-function fizzBuzz(entrada) {
-  if (typeof entrada !== "number") return "Não é um número";
-  if (entrada % 3 === 0 && entrada % 5 === 0) return "FizzBuzz";
-  if (entrada % 3 === 0) return "Fizz";
-  if (entrada % 5 === 0) return "Buzz";
-  return entrada;
-}
-let resultado = fizzBuzz(14);
-console.log(resultado);
+//function fizzBuzz(entrada) {
+//  if (typeof entrada !== "number") return "Não é um número";
+//  if (entrada % 3 === 0 && entrada % 5 === 0) return "FizzBuzz";
+//  if (entrada % 3 === 0) return "Fizz";
+//  if (entrada % 5 === 0) return "Buzz";
+//  return entrada;
+//}
+//let resultado = fizzBuzz(14);
+//console.log(resultado);
 
 // Reverse a string
-let newStr = "";
-function reverseAString(str) {
-  for (let i = str.length - 1; i >= 0; i--) {
-    newStr += str[i];
-    //console.log(newStr);
-  }
-  console.log(newStr);
-}
-let resultado2 = reverseAString("Hello Gama Academy");
+//let newStr = "";
+//function reverseAString(str) {
+//  for (let i = str.length - 1; i >= 0; i--) {
+//    newStr += str[i];
+//console.log(newStr);
+//  }
+//  console.log(newStr);
+//}
+//let resultado2 = reverseAString("Hello Gama Academy");
 
 // converter Celsius em Fahrenheit
-function convertToFahrenheit(valor) {
-  return valor * 1.8 + 32;
-}
-let resultado3 = convertToFahrenheit(3);
-console.log(`O valor em Fahrenheit é ${resultado3}°F`);
+//function convertToFahrenheit(valor) {
+//  return valor * 1.8 + 32;
+//}
+//let resultado3 = convertToFahrenheit(3);
+//console.log(`O valor em Fahrenheit é ${resultado3}°F`);
 
 const listContainer = document.querySelector("[data-lists]");
 const newListForm = document.querySelector("[data-new-list-form]");
@@ -56,7 +56,7 @@ function render() {
     // laço de repetição forEach. vamos percorrer nossa lista. criar uma função anonima e dentro dela passar cada intem do array chamado de list
     const item = document.createElement("li"); //criamos um variável item, acessamos o conteúdo html para inserir um item criado chamado createElement, que é uma lista (li)
     item.classList.add("item"); // adicionamos uma class ao item li de nome item
-    item.innerText = list.nome; //o item li vai receber de texto os itens da lista (list)
+    item.innerText = list; //o item li vai receber de texto os itens da lista (list)
     listContainer.appendChild(item); //em qual elemento mãe do html que vai ficar que é a listContainer (o filho é o item list)
   });
 }
